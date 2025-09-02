27c298b4195b4c2dd30b87e8f27d939bfc5285cd
Aman dhoni <parleaman@gmail.com>
2025-09-02 07:01:35 +0530

    printArray

---
+++ Array/PrintArray.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.Scanner;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0; i<arr.length; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Array is : ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
