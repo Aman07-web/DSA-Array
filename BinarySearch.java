a2aec7b54eea5f450bfe2057ed4de4faab6cdf79
Aman dhoni <parleaman@gmail.com>
2025-09-03 07:02:45 +0530

    BinarySearch

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,20 +1,20 @@
-import java.util.Scanner;
-
 public class BinarySearch {
     public static void main(String[] args) {
-         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of array: ");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
-        for(int i=0; i<arr.length; i++){
-            arr[i]=sc.nextInt();
-        }
-        System.out.println("Enter element u want to search :");
-        int search=sc.nextInt();
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]==search){
-                System.out.println("Element found :"+i);
+        int arr[]={4,6,8,12,14,56};
+        int start=0, end=arr.length-1;
+        int key=12;
+        while(start <= end){
+            int mid=(start+end)/2;
+            if(arr[mid]==key){
+                System.out.println("Element found :"+mid);
+            }
+            if(arr[mid] < key){
+             start=mid+1;
+            }
+            else{
+                end=mid-1;
             }
         }
     }
+    
 }
---
+++ LinearSearch.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0; i<arr.length; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter element u want to search :");
+        int search=sc.nextInt();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==search){
+                System.out.println("Element found :"+i);
+            }
+        }
+    }
+}
