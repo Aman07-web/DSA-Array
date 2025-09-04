d06a8a53d4a04b7152e4fb8809dd645d198a48ff
Aman dhoni <parleaman@gmail.com>
2025-09-04 07:15:04 +0530

    pair of arrsy

---
+++ PairArray.java
@@ -0,0 +1,16 @@
+public class PairArray {
+    public static void printPairs(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            int curr=arr[i];
+            for(int j=i+1; j<arr.length; j++){
+                System.out.print("("+curr+","+arr[j]+")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={2,3,5,7,8};
+        printPairs(arr);
+    }
+    
+}
